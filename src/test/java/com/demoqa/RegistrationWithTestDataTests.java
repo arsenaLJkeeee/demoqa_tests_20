954d96a494bbed31ecd7e82d4677d9a3d6207415
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-06-12 12:18:31 +0300

    fix mistake

--- src/test/java/com/demoqa/RegistrationWithTestDataTests.java
+++ src/test/java/com/demoqa/RegistrationWithTestDataTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistratinWithTestDataTests extends TestBase{
+public class RegistrationWithTestDataTests extends TestBase{
 
 
     @Test
