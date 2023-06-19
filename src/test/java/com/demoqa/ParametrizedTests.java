d0d296c003c753e972a242f74cc24d2fda82ecf7
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-06-19 19:00:05 +0300

    add methodsource

--- src/test/java/com/demoqa/ParametrizedTests.java
+++ src/test/java/com/demoqa/ParametrizedTests.java
@@ -3,7 +3,11 @@ package com.demoqa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -34,5 +38,26 @@ public class ParametrizedTests extends TestBase {
         $(byText(eMail));
         $(byText(currentAddress));
         $(byText(permanentAddress));
+    }
+
+    @MethodSource("dataArguments")
+    void filledTextBoxPageShouldShowCorrectData(String firstName, String eMail, String currentAddress, String permanentAddress) {
+        $("#userName").setValue(firstName);
+        $("#userEmail").setValue(eMail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $(byText(firstName));
+        $(byText(eMail));
+        $(byText(currentAddress));
+        $(byText(permanentAddress));
+    }
+
+    static Stream<Arguments> dataArguments() {
+        return Stream.of(
+                Arguments.of("Alex", "alex@mail.ru", "Industrialnaia street", "Permanent address of Alex"),
+                Arguments.of("Mikhail", "mikhail@mail.ru", "Mikhailovskaya street", "Permanent address of Mikhail")
+        );
     }
 }
