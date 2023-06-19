b8b84d0ff72d404b814877e99d42dad4e1593dc4
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-06-19 18:50:55 +0300

    add first parametrizedtest and parametrizedTestBase

---
+++ src/test/java/com/demoqa/ParametrizedTests.java
@@ -0,0 +1,38 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ParametrizedTests extends TestBase {
+
+    @BeforeEach
+    void openPage() {
+        open("/text-box");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+    }
+
+    @DisplayName("Заполненная форма должна показывать корректные данные при нажатии на submit")
+    @CsvSource(value = {
+            "Alex |  alex@mail.ru | Industrialnaia street | Permanent address of Alex",
+            "Mikhail    |  mikhail@mail.ru | Mikhailovskaya street | Permanent address of Mikhail"
+    },
+            delimiter = '|')
+    @ParameterizedTest
+    void filledTextBoxPageShouldShowCorrectDataAfterSubmitClick(String firstName, String eMail, String currentAddress, String permanentAddress) {
+        $("#userName").setValue(firstName);
+        $("#userEmail").setValue(eMail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+        $(byText(firstName));
+        $(byText(eMail));
+        $(byText(currentAddress));
+        $(byText(permanentAddress));
+    }
+}
