94aac651a85160a8e7a283a67c8a0c36e655ef5f
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-06-30 21:18:46 +0300

    add duration time

--- src/test/java/petPracticeTests/LsTest.java
+++ src/test/java/petPracticeTests/LsTest.java
@@ -21,13 +21,13 @@ public class LsTest extends LsTestBase {
     void redesignCupisWalletClickTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         step("Открываем главную страницу", () -> {open(baseUrl);});
-        step("Принимаем куки", () -> {$(byText("Принять все")).shouldBe(visible, Duration.ofSeconds(10000)).click();});
-        step("Кликаем по кнопке Вход", () -> {$(byText("Вход")).shouldBe(visible, Duration.ofSeconds(10000)).click();});
-        step("Вводим номер телефона", () -> {$("#mobilePhone").shouldBe(visible, Duration.ofSeconds(10000)).setValue("1014203177");});
-        step("Вводим пароль", () -> {$("#password").shouldBe(visible, Duration.ofSeconds(10000)).setValue("123123eE");});
+        step("Принимаем куки", () -> {$(byText("Принять все")).shouldBe(visible, Duration.ofSeconds(13000)).click();});
+        step("Кликаем по кнопке Вход", () -> {$(byText("Вход")).shouldBe(visible, Duration.ofSeconds(13000)).click();});
+        step("Вводим номер телефона", () -> {$("#mobilePhone").shouldBe(visible, Duration.ofSeconds(13000)).setValue("1014203177");});
+        step("Вводим пароль", () -> {$("#password").shouldBe(visible, Duration.ofSeconds(13000)).setValue("123123eE");});
         step("Кликаем по кнопке Войти", () -> {$(byText("Войти")).shouldBe(visible).click();});
-        $(".bottom-menu_eG-q").lastChild().shouldBe(visible, Duration.ofSeconds(10000)).click();
-        $(".wallet__buttons-e1c20c").$(byText("Пополнить")).shouldBe(visible, Duration.ofSeconds(10000)).click();
-        $(byText("Кошелек ЦУПИС")).shouldBe(visible, Duration.ofSeconds(10000)).click(ClickOptions.usingJavaScript());
+        $(".bottom-menu_eG-q").lastChild().shouldBe(visible, Duration.ofSeconds(13000)).click();
+        $(".wallet__buttons-e1c20c").$(byText("Пополнить")).shouldBe(visible, Duration.ofSeconds(13000)).click();
+        $(byText("Кошелек ЦУПИС")).shouldBe(visible, Duration.ofSeconds(13000)).click(ClickOptions.usingJavaScript());
     }
 }
