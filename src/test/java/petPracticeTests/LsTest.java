a942d9a822ab623e15c5d3054282de7e6055e5d7
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-05 16:15:54 +0300

    add step

--- src/test/java/petPracticeTests/LsTest.java
+++ src/test/java/petPracticeTests/LsTest.java
@@ -28,7 +28,7 @@ public class LsTest extends LsTestBase {
         step("Вводим пароль", () -> {$("#password").shouldBe(visible, Duration.ofSeconds(13000)).setValue("123123eE");});
         step("Кликаем по кнопке Войти", () -> {$(byText("Войти")).shouldBe(visible).click();});
         $(".bottom-menu_eG-q").lastChild().shouldBe(visible, Duration.ofSeconds(13000)).click();
-        $(".wallet__buttons-e1c20c").$(byText("Пополнить")).shouldBe(visible, Duration.ofSeconds(13000)).click();
+        step("Кликаем по кнопке Пополнить", () -> {$(".wallet__buttons-e1c20c").$(byText("Пополнить")).shouldBe(visible, Duration.ofSeconds(13000)).click();});
         step("Кликаем по каналу ЦУПИС", () -> {$(byText("Кошелек ЦУПИС")).shouldBe(visible, Duration.ofSeconds(13000)).click(ClickOptions.usingJavaScript());});
     }
 }
