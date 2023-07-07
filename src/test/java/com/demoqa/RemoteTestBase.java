d8af688240b2f70240c7b9824054cfae292160a8
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-07 21:09:41 +0300

    polish up

--- src/test/java/com/demoqa/RemoteTestBase.java
+++ src/test/java/com/demoqa/RemoteTestBase.java
@@ -18,7 +18,6 @@ public class RemoteTestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-//        Configuration.browser = "chrome";
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
